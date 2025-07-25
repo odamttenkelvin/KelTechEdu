c9e750b4373474a23f30a833d595e8d0b854cc32
KelCyberTech <kelvinodamtten@gmail.com>
2025-07-25 19:43:52 +0100

    Added React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>School Management System</h1>
+      <p>Frontend is working!</p>
+    </div>
+  );
+}
+
+export default App;
